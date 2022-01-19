18e0b90fa323bb9cf7d4366d8bc778db20f32016
Ivan Boldyrev <lispnik@gmail.com>
2022-01-19 17:44:38 +0700

    Final polishing: kinda docs, tests

--- README.md
+++ README.md
@@ -7,18 +7,19 @@ This package is to be used for interrupting IO in conjunction with
 signal handling crates like `ctrlc`.  These crates do handle received
 signals and allow to set some flag, but if application code is performing
 IO, it will not able to check the flag until it completes.  The Unix-like
-OSes do interrupt IO operations on IO, returning `EINT` error, but std
-utility methods sliently restart such operations.
+OSes do interrupt IO operations on signals, returning `EINT` errno (aka 
+`ErrorKind::Interrupted`), but `std::io` utility methods sliently restart
+such operations.
 
 The `Interruptable` wrapper for `std::io::Read` and `std::io::Write` checks
 the flag (`std::sync::atomic::AtomicBool`) on each `read` or `write` operation,
 and when it sees that flag is set, it returns `std::io::Error` (standard error
 for the IO operations) with `std::io::ErrorKind::Other` and nested error of
-`ErrorKind::Interrupted`.
+`ErrorKind::Interrupted`, thus hiding it from the retrying logic.
 
 Life without `interruptabe`:
 
-``` rust
+``` rust,ignore
 use std::io;
 use std::sync::atomic;
 use ctrlc;
@@ -26,9 +27,9 @@ use ctrlc;
 let interrupt_flag = atomic::Arc::new(atomic::AtomicBool::new(false));
 let interrput_flag2 = interrupt_flag.clone();
 
-let _ = ctrlc::set_handler(move || {
+ctrlc::set_handler(move || {
     interrput_flag2.store(true, std::sync::atomic::Ordering::SeqCst);
-});
+}).unwrap();
 
 let file = io::BufReader::new(
    std::fs::File::open("/path/to/slow/media/data.data")?
@@ -50,7 +51,7 @@ for line in file.lines() {
 
 Life with `interruptabe`:
 
-``` rust
+``` rust,ignore
 use std::io;
 use std::sync::atomic;
 use ctrlc;
@@ -58,9 +59,9 @@ use ctrlc;
 let interrupt_flag = atomic::Arc::new(atomic::AtomicBool::new(false));
 let interrput_flag2 = interrupt_flag.clone();
 
-let _ = ctrlc::set_handler(move || {
+ctrlc::set_handler(move || {
     interrput_flag2.store(true, std::sync::atomic::Ordering::SeqCst);
-});
+}).unwrap();
 
 let file = io::BufReader::new(
     // Work both for Read and Write.
@@ -74,8 +75,9 @@ let my_precious_resource = MyResource::new();
 
 for line in file.lines() {
     // When the signal will arrive and flag is set, line will be Err(...)
-    // immediately, thus you will be able to gracefully shutown
-    // your application: my_precious_resource is destroyed in standard way.
+    // immediately, thus you will be able to handle it and gracefully
+    // shutdown your application: my_precious_resource is destroyed
+    // in the standard way, by Drop::drop.
     let line = line?;
     ...
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,32 @@
+/* Copyright 2022 Ivan Boldyrev
+ *
+ * Licensed under the MIT License.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#![doc = include_str!("../README.md")]
 use std::{
     io,
     sync::atomic::{AtomicBool, Ordering},
 };
 
+/** See crate-level documentation for more info. */
 pub struct Interruptable<IO, H> {
     inner: IO,
     interrupt_flag: H,
@@ -66,5 +90,203 @@ impl<IO: io::Write, H: AsRef<AtomicBool>> io::Write for Interruptable<IO, H> {
         } else {
             self.inner.flush().map_err(|e| self.check_again(e))
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::{
+        io::{self, ErrorKind, Read, Write},
+        sync::{
+            atomic::{AtomicBool, Ordering},
+            Arc,
+        },
+    };
+
+    use crate::Interruptable;
+
+    struct Mock {
+        value: Option<io::Result<Vec<u8>>>,
+        interrupt: Option<Arc<AtomicBool>>,
+    }
+
+    impl Mock {
+        fn new(value: io::Result<Vec<u8>>, interrupt: Option<Arc<AtomicBool>>) -> Self {
+            Self {
+                value: Some(value),
+                interrupt,
+            }
+        }
+    }
+
+    impl Read for Mock {
+        fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+            if let Some(int) = &self.interrupt {
+                int.store(true, Ordering::SeqCst);
+            }
+            match self.value.take() {
+                None => Err(io::Error::from(io::ErrorKind::UnexpectedEof)),
+                Some(Ok(value)) => {
+                    let len = std::cmp::min(buf.len(), value.len());
+                    buf.copy_from_slice(&value[..len]);
+                    Ok(len)
+                }
+                Some(Err(e)) => Err(e),
+            }
+        }
+    }
+
+    impl Write for Mock {
+        fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+            match self.value.take() {
+                Some(Ok(_)) => Ok(buf.len()),
+                Some(Err(e)) => Err(e),
+                None => Err(io::Error::from(ErrorKind::BrokenPipe)),
+            }
+        }
+
+        fn flush(&mut self) -> io::Result<()> {
+            Ok(())
+        }
+    }
+
+    #[test]
+    fn test_read_normal() {
+        let flag = Arc::new(AtomicBool::new(false));
+        let flag2 = flag.clone();
+        let mut inp = Interruptable::new(Mock::new(Ok(vec![42; 100]), None), flag2);
+        let mut buf = vec![0; 42];
+
+        assert!(matches!(inp.read(&mut buf), Ok(42)));
+        assert_eq!(buf, vec![42; 42]);
+    }
+
+    #[test]
+    fn test_read_error() {
+        let flag = Arc::new(AtomicBool::new(false));
+        let mut inp = Interruptable::new(
+            Mock::new(Err(io::Error::from(io::ErrorKind::BrokenPipe)), None),
+            flag,
+        );
+        let mut buf = vec![0; 42];
+
+        let e = inp.read(&mut buf).unwrap_err();
+        assert_eq!(e.kind(), io::ErrorKind::BrokenPipe);
+    }
+
+    #[test]
+    fn test_read_pre_interrupt() {
+        let flag = Arc::new(AtomicBool::new(false));
+        let flag2 = flag.clone();
+        let mut inp = Interruptable::new(Mock::new(Ok(vec![42; 100]), None), flag2);
+        let mut buf = vec![0; 42];
+        flag.store(true, Ordering::SeqCst);
+
+        let e = inp.read(&mut buf).unwrap_err();
+        assert_eq!(e.kind(), io::ErrorKind::Other);
+        assert!(e.get_ref().is_some());
+    }
+
+    #[test]
+    fn test_read_incall_interrupt() {
+        let flag = Arc::new(AtomicBool::new(false));
+        let flag2 = flag.clone();
+        let flag3 = flag.clone();
+        let mut inp = Interruptable::new(
+            Mock::new(
+                Err(io::Error::from(io::ErrorKind::Interrupted)),
+                Some(flag3),
+            ),
+            flag2,
+        );
+        let mut buf = vec![0; 42];
+        flag.store(true, Ordering::SeqCst);
+
+        let e = inp.read(&mut buf).unwrap_err();
+        assert_eq!(e.kind(), io::ErrorKind::Other);
+        assert!(e.get_ref().is_some());
+    }
+
+    #[test]
+    fn test_read_unhandled_interrupt() {
+        let flag = Arc::new(AtomicBool::new(false));
+        let flag2 = flag.clone();
+        let mut inp = Interruptable::new(
+            Mock::new(Err(io::Error::from(io::ErrorKind::Interrupted)), None),
+            flag2,
+        );
+        let mut buf = vec![0; 42];
+
+        let e = inp.read(&mut buf).unwrap_err();
+        assert_eq!(e.kind(), io::ErrorKind::Interrupted);
+    }
+
+    #[test]
+    fn test_write_normal() {
+        let flag = Arc::new(AtomicBool::new(false));
+        let mut inp = Interruptable::new(Mock::new(Ok(vec![42; 0]), None), flag);
+        let buf = vec![0; 42];
+
+        assert!(matches!(inp.write(&buf), Ok(42)));
+    }
+
+    #[test]
+    fn test_write_error() {
+        let flag = Arc::new(AtomicBool::new(false));
+        let mut inp = Interruptable::new(
+            Mock::new(Err(io::Error::from(io::ErrorKind::BrokenPipe)), None),
+            flag,
+        );
+        let buf = vec![0; 42];
+
+        let e = inp.write(&buf).unwrap_err();
+        assert_eq!(e.kind(), io::ErrorKind::BrokenPipe);
+    }
+
+    #[test]
+    fn test_write_pre_interrupt() {
+        let flag = Arc::new(AtomicBool::new(false));
+        let flag2 = flag.clone();
+        let mut inp = Interruptable::new(Mock::new(Ok(vec![42; 100]), None), flag2);
+        let buf = vec![0; 42];
+        flag.store(true, Ordering::SeqCst);
+
+        let e = inp.write(&buf).unwrap_err();
+        assert_eq!(e.kind(), io::ErrorKind::Other);
+        assert!(e.get_ref().is_some());
+    }
+
+    #[test]
+    fn test_write_incall_interrupt() {
+        let flag = Arc::new(AtomicBool::new(false));
+        let flag2 = flag.clone();
+        let flag3 = flag.clone();
+        let mut inp = Interruptable::new(
+            Mock::new(
+                Err(io::Error::from(io::ErrorKind::Interrupted)),
+                Some(flag3),
+            ),
+            flag2,
+        );
+        let buf = vec![0; 42];
+        flag.store(true, Ordering::SeqCst);
+
+        let e = inp.write(&buf).unwrap_err();
+        assert_eq!(e.kind(), io::ErrorKind::Other);
+        assert!(e.get_ref().is_some());
+    }
+
+    #[test]
+    fn test_write_unhandled_interrupt() {
+        let flag = Arc::new(AtomicBool::new(false));
+        let flag2 = flag.clone();
+        let mut inp = Interruptable::new(
+            Mock::new(Err(io::Error::from(io::ErrorKind::Interrupted)), None),
+            flag2,
+        );
+        let buf = vec![0; 42];
+
+        let e = inp.write(&buf).unwrap_err();
+        assert_eq!(e.kind(), io::ErrorKind::Interrupted);
     }
 }
